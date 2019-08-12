23905340778236dd4f190d05d77a8fe089e10ad1
Vlad Bulanyy <vladbulanyy@gmail.com>
2019-08-12 07:45:25 +0300

    Added delete functional

--- Library/Scripts/app/Main.js
+++ Library/Scripts/app/Main.js
@@ -1,4 +1,120 @@
-﻿// Class to represent a row in the seat reservations grid
+﻿var books = [
+    {
+        name: "Jenseits von Gut und Böse: prelude to a philosophy of the future",
+        date: new Date(2011, 0, 1).toLocaleDateString(),
+        autor: "Friedrich Nietzsche",
+        rate: 5,
+        pages: 150,
+        autorAtrr: "Friedrich Wilhelm Nietzsche (1844-1900)"
+    }, {
+        name: "Heidi ",
+        date: new Date('October 05, 1815').toLocaleDateString(),
+        autor: "Johanna Spyri",
+        rate: 2,
+        pages: 789,
+        autorAtrr: "Around 1875"
+    }, {
+        name: "Meus Artigos",
+        date: new Date('March 17, 1874').toLocaleDateString(),
+        autor: "Jacob Burckhardt",
+        rate: 18,
+        pages: 550,
+        autorAtrr: "Phaidon-Verlag"
+    }, {
+        name: "The Sketch Book",
+        date: new Date('May 08, 1805').toLocaleDateString(),
+        autor: "Washington Irving",
+        rate: 5,
+        pages: 258,
+        autorAtrr: "William Lyon Phelps (1865-1943)"
+    }, {
+        name: "Jenseits von Gut und Böse: prelude to a philosophy of the future",
+        date: new Date('December 17, 1905').toLocaleDateString(),
+        autor: "Friedrich Nietzsche",
+        rate: 5,
+        pages: 150,
+        autorAtrr: "Friedrich Wilhelm Nietzsche (1844-1900)"
+    }, {
+        name: "Jenseits von Gut und Böse: prelude to a philosophy of the future",
+        date: new Date('December 17, 1905').toLocaleDateString(),
+        autor: "Friedrich Nietzsche",
+        rate: 5,
+        pages: 150,
+        autorAtrr: "Friedrich Wilhelm Nietzsche (1844-1900)"
+    }, {
+        name: "Jenseits von Gut und Böse: prelude to a philosophy of the future",
+        date: new Date('December 17, 1905').toLocaleDateString(),
+        autor: "Friedrich Nietzsche",
+        rate: 5,
+        pages: 150,
+        autorAtrr: "Friedrich Wilhelm Nietzsche (1844-1900)"
+    }, {
+        name: "Jenseits von Gut und Böse: prelude to a philosophy of the future",
+        date: new Date('December 17, 1905').toLocaleDateString(),
+        autor: "Friedrich Nietzsche",
+        rate: 5,
+        pages: 150,
+        autorAtrr: "Friedrich Wilhelm Nietzsche (1844-1900)"
+    }, {
+        name: "Jenseits von Gut und Böse: prelude to a philosophy of the future",
+        date: new Date('December 17, 1905').toLocaleDateString(),
+        autor: "Friedrich Nietzsche",
+        rate: 5,
+        pages: 150,
+        autorAtrr: "Friedrich Wilhelm Nietzsche (1844-1900)"
+    }, {
+        name: "Jenseits von Gut und Böse: prelude to a philosophy of the future",
+        date: new Date('December 17, 1905').toLocaleDateString(),
+        autor: "Friedrich Nietzsche",
+        rate: 5,
+        pages: 150,
+        autorAtrr: "Friedrich Wilhelm Nietzsche (1844-1900)"
+    }, {
+        name: "Jenseits von Gut und Böse: prelude to a philosophy of the future",
+        date: new Date('December 17, 1905').toLocaleDateString(),
+        autor: "Friedrich Nietzsche",
+        rate: 5,
+        pages: 150,
+        autorAtrr: "Friedrich Wilhelm Nietzsche (1844-1900)"
+    }, {
+        name: "Jenseits von Gut und Böse: prelude to a philosophy of the future",
+        date: new Date('December 17, 1905').toLocaleDateString(),
+        autor: "Friedrich Nietzsche",
+        rate: 5,
+        pages: 150,
+        autorAtrr: "Friedrich Wilhelm Nietzsche (1844-1900)"
+    }, {
+        name: "Jenseits von Gut und Böse: prelude to a philosophy of the future",
+        date: new Date('December 17, 1905').toLocaleDateString(),
+        autor: "Friedrich Nietzsche",
+        rate: 5,
+        pages: 150,
+        autorAtrr: "Friedrich Wilhelm Nietzsche (1844-1900)"
+    }, {
+        name: "Jenseits von Gut und Böse: prelude to a philosophy of the future",
+        date: new Date('December 17, 1905').toLocaleDateString(),
+        autor: "Friedrich Nietzsche",
+        rate: 5,
+        pages: 150,
+        autorAtrr: "Friedrich Wilhelm Nietzsche (1844-1900)"
+    }, {
+        name: "Jenseits von Gut und Böse: prelude to a philosophy of the future",
+        date: new Date('December 17, 1905').toLocaleDateString(),
+        autor: "Friedrich Nietzsche",
+        rate: 5,
+        pages: 150,
+        autorAtrr: "Friedrich Wilhelm Nietzsche (1844-1900)"
+    }, {
+        name: "Jenseits von Gut und Böse: prelude to a philosophy of the future",
+        date: new Date('December 17, 1905').toLocaleDateString(),
+        autor: "Friedrich Nietzsche",
+        rate: 5,
+        pages: 150,
+        autorAtrr: "Friedrich Wilhelm Nietzsche (1844-1900)"
+    },
+];
+
+// Class to represent a row in the seat reservations grid
 function SeatReservation(name, initialMeal) {
     var self = this;
     self.name = name;
@@ -10,35 +126,41 @@ function SeatReservation(name, initialMeal) {
     });
 }
 
+
+
 // Overall viewmodel for this screen, along with initial state
-function ReservationsViewModel() {
+function InitBooksData() {
     var self = this;
 
     // Non-editable catalog data - would come from the server
-    self.availableMeals = [
-        { mealName: "Standard (sandwich)", price: 0 },
-        { mealName: "Premium (lobster)", price: 34.95 },
-        { mealName: "Ultimate (whole zebra)", price: 290 }
-    ];
+    self.books = ko.observableArray(books);
 
     // Editable data
     self.seats = ko.observableArray([
-        new SeatReservation("Steve", self.availableMeals[0]),
-        new SeatReservation("Bert", self.availableMeals[1])
+        //new SeatReservation("Steve", self.availableMeals[0]),
+        //new SeatReservation("Bert", self.availableMeals[1])
     ]);
 
     self.addSeat = function () {
-        self.seats.push(new SeatReservation("", self.availableMeals[0]));
+        //self.seats.push(new SeatReservation("", self.availableMeals[0]));
+    }
+    self.empArray = ko.observableArray(['Scott', 'James', 'Jordan', 'Lee',
+        'RoseMary', 'Kathie']);
+
+    self.removeBook = function () {
+        self.books.remove(this)
+        console.log(self.empArray())
+        self.empArray.remove('James')
+        console.log(self.empArray())
     }
 
-    self.removeSeat = function (seat) { self.seats.remove(seat) }
-
     self.totalSurcharge = ko.computed(function () {
-        var total = 0;
-        for (var i = 0; i < self.seats().length; i++)
-            total += self.seats()[i].meal().price;
-        return total;
+        //var total = 0;
+        //for (var i = 0; i < self.seats().length; i++)
+        //    total += self.seats()[i].meal().price;
+        //return total;
     });
 }
 
-ko.applyBindings(new ReservationsViewModel());
+ko.applyBindings(new InitBooksData());
+
